32dddb7abb1c3d688dff2c38d5c08181a4aae2d9
Favour Afolayan <favourafolayan@gmail.com>
2020-01-29 18:35:09 +0100

    feature(events): users can view all events and create new one
    this closes #3 #4

---
+++ resources/views/events/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+    <div class="col-md-10">
+        <h2>Events</h2>
+        <a class="btn btn-primary" href="/events/create">Create Event</a>
+        <br>
+        <div class="row">
+            @foreach($events as $event)
+                @include('events.event')
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
